8da2e2b79854d50d2ebc6e4679603e983d584719
Grigorii <144004392+bluvvis@users.noreply.github.com>
2025-04-29 14:45:47 +0300

    Update producer.py

--- producer.py
+++ producer.py
@@ -1,1 +1,17 @@
+from kafka import KafkaProducer
+import json
+
+producer = KafkaProducer(
+    bootstrap_servers='localhost:9092',
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+print("Kafka Chat Producer started.")
+username = input("Enter your username: ")
+
+while True:
+    message = input("Message: ")
+    data = {'user': username, 'message': message}
+    producer.send('chat', value=data)
+    print("Message sent.")
 
