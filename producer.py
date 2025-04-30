ad396bd2f4dd82b4863914c01a7f5bdd6b1ff416
Borodum <batraz@dzesov.ru>
2025-04-30 21:29:21 +0300

    added system notification of users entering and leaving chat

--- producer.py
+++ producer.py
@@ -1,5 +1,6 @@
 from kafka import KafkaProducer
 import json
+import atexit
 
 producer = KafkaProducer(
     bootstrap_servers='localhost:9092',
@@ -9,9 +10,23 @@ producer = KafkaProducer(
 print("Kafka Chat Producer started.")
 username = input("Enter your username: ")
 
-while True:
-    message = input("Message: ")
-    data = {'user': username, 'message': message}
-    producer.send('chat', value=data)
-    print("Message sent.")
+producer.send('chat', value={'user': 'system', 'message': f'{username} joined the chat.'})
+producer.flush()
 
+
+def send_leave_message():
+    producer.send('chat', value={'user': 'system', 'message': f'{username} left the chat.'})
+    producer.flush()
+    producer.close()
+
+
+atexit.register(send_leave_message)
+try:
+    while True:
+        message = input("Message: ")
+        data = {'user': username, 'message': message}
+        producer.send('chat', value=data)
+        print("Message sent.")
+except KeyboardInterrupt:
+    print("\nExiting...")
+    send_leave_message()
